2ea4cef83d42f00384276b6b4c81ea52facd7e1b
Aiowarrior <uma8@scarletmail.rutgers.edu>
2023-09-21 03:10:01 -0400

    added script to basket for score

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -1,8 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour {
+    [Header("Set Dynamically")]
+public Text scoreGT; // a
+void Start() {
+// Find a reference to the ScoreCounter GameObject
+GameObject scoreGO = GameObject.Find("ScoreCounter"); // b
+// Get the Text Component of that GameObject
+scoreGT = scoreGO.GetComponent<Text>(); // c
+// Set the starting number of points to 0
+scoreGT.text = "0";
+}
 void Update () {
 // Get the current screen position of the mouse from Input
 Vector3 mousePos2D = Input.mousePosition; // a
@@ -20,6 +31,12 @@ void OnCollisionEnter( Collision coll ) { // a
 GameObject collidedWith = coll.gameObject; // b
 if ( collidedWith.tag == "Apple" ) { // c
 Destroy( collidedWith );
+// Parse the text of the scoreGT into an int
+int score = int.Parse( scoreGT.text ); // d
+// Add points for catching the apple
+score += 100;
+// Convert the score back to a string and display it
+scoreGT.text = score.ToString();
 }
 }
 }
