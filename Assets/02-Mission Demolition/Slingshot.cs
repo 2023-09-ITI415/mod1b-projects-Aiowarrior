5a7bc8e287451c0506bb9a1241756eb6a5c8056f
Aiowarrior <uma8@scarletmail.rutgers.edu>
2023-09-27 22:49:58 -0400

    Created halo for launchpoint

--- Assets/02-Mission Demolition/Slingshot.cs
+++ Assets/02-Mission Demolition/Slingshot.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+    public GameObject launchPoint;
+void Awake() {
+Transform launchPointTrans = transform.Find("LaunchPoint"); // a
+launchPoint = launchPointTrans.gameObject;
+launchPoint.SetActive( false ); // b
+}
 void OnMouseEnter() {
 print("Slingshot:OnMouseEnter()");
+launchPoint.SetActive( true );
 }
 void OnMouseExit() {
 print("Slingshot:OnMouseExit()");
+launchPoint.SetActive( false );
 }
 }
 
