40d6c64aa4ff5d2969eee66eaa9cadb5ccdb175c
Aiowarrior <uma8@scarletmail.rutgers.edu>
2023-09-28 00:04:08 -0400

    created script to make the bounce of the projectile more smooth

--- Assets/02-Mission Demolition/FollowCam.cs
+++ Assets/02-Mission Demolition/FollowCam.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour {
 static public GameObject POI; // The static point of interest // a
+[Header("Set in Inspector")]
+public float easing = 0.05f;
+public Vector2 minXY = Vector2.zero;
 [Header("Set Dynamically")]
 public float camZ; // The desired Z pos of the camera
 void Awake() {
@@ -14,9 +17,13 @@ void FixedUpdate () {
 if (POI == null) return; // return if there is no poi // b
 // Get the position of the poi
 Vector3 destination = POI.transform.position;
+destination.x = Mathf.Max( minXY.x, destination.x );
+destination.y = Mathf.Max( minXY.y, destination.y );
+destination = Vector3.Lerp(transform.position, destination, easing);
 // Force destination.z to be camZ to keep the camera far enough away
 destination.z = camZ;
 // Set the camera to the destination
 transform.position = destination;
+Camera.main.orthographicSize = destination.y + 10;
 }
 }
