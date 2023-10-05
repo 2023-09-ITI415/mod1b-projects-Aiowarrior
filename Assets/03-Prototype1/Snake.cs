dc6bc4b38ffa9d7b993b99acdf05d2a4d46868d8
Aiowarrior <uma8@scarletmail.rutgers.edu>
2023-10-05 04:47:28 -0400

    fixed my error in my snake code was missing a reference and also added a reset function when hitting the wall or the snake's body.

--- Assets/03-Prototype1/Snake.cs
+++ Assets/03-Prototype1/Snake.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
 public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
 
-    private List<Transform> _segments;
+    private List<Transform> _segments = new List<Transform>();
 
     public Transform segmentPrefab;
 
+    public int initialSize = 4;
+
     private void Start()
     {
-        _segments = new List<Transform>();
-        _segments.Add(this.transform);
+        ResetState();
     }
 
     private void Update()
@@ -32,7 +32,7 @@ public class Snake : MonoBehaviour
 
     private void FixedUpdate() 
     {
-        for (int i = _segments.count -1; i > 0; i-- )
+        for (int i = _segments.Count -1; i > 0; i-- )
         {
             _segments[i].position = _segments[i - 1].position;
         }
@@ -47,15 +47,32 @@ public class Snake : MonoBehaviour
     private void Grow()
     {
         Transform segment = Instantiate(this.segmentPrefab);
-        segment.position = _segments[_segments.count - 1].position;
+        segment.position = _segments[_segments.Count - 1].position;
 
         _segments.Add(segment);
+    }
+    private void ResetState()
+    {
+        for (int i = 1; i < _segments.Count; i++ ) {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        for (int i = 1; i < this.initialSize; i++ ) {
+            _segments.Add(Instantiate(this.segmentPrefab));
+        }
+
+        this.transform.position = Vector3.zero;
     }
 
      private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Food") {
             Grow();
+        } else if (other.tag == "Wall") {
+            ResetState();
         }
     }
 }
